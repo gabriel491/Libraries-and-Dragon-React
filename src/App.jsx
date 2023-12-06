5269aaf3895aba320c2327a7af081800471699d6
Gabriel Macêdo <gm8825123@gmail.com>
2023-12-06 09:57:07 -0300

    Navegação entre Login e Forum Implementada

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
-import './App.css'
 import React from "react";
-import FormLogin from './components/FormLogin';
-import Forum from './components/Forum';
+import FormLogin from "./components/FormLogin"; // Import the FormLogin component
+import Forum from "./components/Forum";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
-// npm run dev
 function App() {
-
   return (
-    <>
-      <div>
-        {/* <FormLogin /> */}
-        <Forum />
-      </div>
-    </>
-  )
+    <Router>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/forum">Forum</Link>
+        </li>
+      </ul>
+
+      <Routes>
+        <Route exact path="/" element={<FormLogin />} />
+        <Route path="/forum" element={<Forum />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
